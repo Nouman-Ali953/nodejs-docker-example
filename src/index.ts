68772b01668ca7137a4511bbc832d7ac7a87dba4
Nouman-Ali953 <nauman.9533@gmail.com>
2024-12-27 12:05:10 +0500

    setting up prometeus

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,18 @@
 import express from "express";
 import router from "./routes/router";
-
+import client from 'prom-client'
 const app = express();
-const PORT = process.env.PORT ?? 3000;
+const PORT = process.env.PORT ?? 3001;
+
+const defaultMetrices = client.collectDefaultMetrics
+
+defaultMetrices({ register: client.register})
+
+app.get('/metrices', async(req,res)=>{
+    res.setHeader('Content-Type', client.register.contentType)
+    const metrices = await client.register.metrics()
+    res.send(metrices)
+})
 
 app.use("/", router);
 
